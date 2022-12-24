c4d49aa85f4ca61a89a8f2f22f82242efd633eee
thien02 <106140494+thien02@users.noreply.github.com>
2022-12-24 14:34:56 +0700

    Add files via upload

---
+++ ProcedureList.sql
@@ -0,0 +1,215 @@
+use DBMS_ThucHanh_Nhom9 
+go
+-----------------------------------------------------------------------------------------
+-- Thêm nhân viên mới
+create procedure insertNV
+	@HoTen nvarchar(50),
+	@SoDT varchar(15),
+	@DiaChi nvarchar(150),
+	@Email nvarchar(50)
+as
+begin tran
+	if exists (select * from THONGTINCANHAN where SoDienThoai = @SoDT) 
+	begin
+		raiserror (N'Số điện thoại đã tồn tại', 10, 1)
+		rollback tran
+	end
+
+	else
+	begin
+		insert into THONGTINCANHAN (HoTen, SoDienThoai, DiaChi, Email) values (@HoTen, @SoDT, @DiaChi, @Email)
+		insert into NHANVIEN (ID) select tt.ID from THONGTINCANHAN tt where tt.SoDienThoai = @SoDT
+		commit tran
+	end
+go
+
+--exec insertNV @HoTen = 'LYN', @SoDT = '0918319976', @DiaChi = 'Le Loi', @Email = Null
+--go
+
+-- Update thông tin cá nhân của nhân viên
+create procedure updateNV
+	@MaNV int,
+	@HoTen nvarchar(50),
+	@SoDienThoai varchar(15),
+	@DiaChi nvarchar(150),
+	@Email varchar(50)
+as
+begin tran
+	UPDATE THONGTINCANHAN set HoTen=@HoTen, SoDienThoai=@SoDienThoai, DiaChi=@DiaChi, Email=@Email 
+	where ID=(select NV.ID from NHANVIEN NV where NV.MaNV=@MaNV)
+	commit tran
+go
+
+--select * from THONGTINCANHAN tt, NHANVIEN nv where nv.id = tt.id and nv.MaNV = 278974
+--exec updateNV 278974, N'Quang Trường', '0854693777', 'Test', 'aaa'
+--go
+
+-- Tìm nhân viên với số điện thoại cho trước
+create procedure lookupNV_SDT
+	@SDT varchar(15)
+as
+begin tran
+	select NV.MANV, TT.HOTEN, TT.SoDienThoai,TT.DIACHI,TT.EMAIL 
+	from NHANVIEN NV, THONGTINCANHAN TT 
+	where TT.SoDienThoai = @SDT and NV.ID = TT.ID
+	commit tran
+go
+
+--exec lookupNV_SDT N'07624466063'
+--go
+
+-- Xóa 1 nhân viên với mã NV và số điện thoại cho trước
+create procedure deleteNV
+	@MaNV int,
+	@SoDienThoai nvarchar(50)
+as
+begin tran
+	Delete NHANVIEN where MANV=@MaNV
+	Delete THONGTINCANHAN where SoDienThoai = @SoDienThoai
+commit tran
+go
+
+--exec deleteNV 278974, '0854693777'
+--go
+
+-- Xem danh sách NV với n dòng 
+create proc ViewStaffListWith_n_Rows
+	@offset int,
+	@rows int
+As
+begin tran
+	begin
+		select nv.MaNV,tt.HoTen,tt.SoDienThoai,tt.DiaChi,tt.Email 
+		from NHANVIEN nv, THONGTINCANHAN tt 
+		where nv.ID=tt.ID 
+		Order by nv.MaNV,tt.HoTen,tt.SoDienThoai,tt.DiaChi,tt.Email offset @offset rows fetch next @rows rows only
+		commit tran
+	end
+go
+
+-- Xem danh sách SP với n dòng  
+create proc ViewProductListWith_n_Rows
+	@offset int,
+	@rows int
+As
+begin tran
+	begin
+		select * from SANPHAM 
+		order by MaSP offset @offset rows fetch next @rows rows only
+		commit tran
+	end
+go
+--exec ViewProductListWith_n_Rows 0,10
+--go
+
+-- Xem SP với mã sản phẩm cho trước
+create procedure lookupSP
+	@MaSP int
+as
+begin tran
+	select * from SANPHAM where MaSp = @MaSP
+commit tran
+go
+
+-- Xóa SP với mã sản phẩm cho trước
+create procedure deleteSP
+	@MaSP int
+as
+begin tran
+	delete from SANPHAM where MaSP = @MaSP
+commit tran
+go
+
+-- Update SP
+create procedure updateSP
+	@MaSP int,
+	@TenSP nvarchar(150),
+	@Gia bigint,
+	@MoTa nvarchar(200)
+as
+begin tran
+	update SANPHAM set TenSP = @TenSP, Gia = @Gia, MoTa = @MoTa where MaSP = @MaSP
+commit tran
+go
+
+-- Thêm SP
+create procedure addSP
+	@TenSP nvarchar(150),
+	@Gia bigint,
+	@MoTa nvarchar(200)
+as
+begin tran
+	insert into SANPHAM (TenSP, Gia, MoTa) values (@TenSP, @Gia, @MoTa)
+	commit tran
+go
+
+-- Hủy Đơn hàng
+create or alter procedure Cancel_Order
+	@MaDH int
+as
+begin tran
+	update DONHANG set TinhTrang = N'Đã hủy' where MaDH = @MaDH
+commit tran
+
+--xem danh sách đối tác với n dòng
+create proc ViewPartnerListWith_n_Rows
+	@offset int,
+	@rows int
+As
+begin tran
+	begin
+		select * from DOITAC 
+		order by MaDT offset @offset rows fetch next @rows rows only
+		commit tran
+	end
+go
+
+--Them DoiTac
+--create Or alter procedure addDT
+--	@TenDT nvarchar(150),
+--	@NguoiDD nvarchar(100),
+--	@TP nvarchar(60),
+--	@Quan nvarchar(60),
+--	@SoCN int,
+--	@SLDonHang int,
+--	@LoaiHang nvarchar(50),
+--	@DIACHIKD nvarchar(150),
+--	@SDT nvarchar(12)
+
+--as
+--begin tran
+--	if exists (select * from DOITAC where SoDienThoai = @SDT) 
+--		begin
+--			raiserror (N'Số điện thoại đã tồn tại', 10, 1)
+--			rollback tran
+--		end
+--	else if exists (select * from DOITAC where DiaChiKinhDoanh = @DIACHIKD) 
+--		begin
+--			raiserror (N'Địa Chỉ kinh doanh đã được đăng ký', 10, 1)
+--			rollback tran
+--		end
+--	else if exists (select * from DOITAC where TenDT = @TenDT) 
+--		begin
+--			raiserror (N'Đối tác đã được đăng ký', 10, 1)
+--			rollback tran
+--		end
+--	else
+--		begin
+--			insert into DOITAC(TenDT,NguoiDaiDien,ThanhPho,Quan,SoChiNhanh,SLDonHang,LoaiHang,DiaChiKinhDoanh,SoDienThoai) values(@TenDT,@NguoiDD,@TP,@Quan,@SoCN,@SLDonHang,@LoaiHang,@DIACHIKD,@SDT)
+--			commit tran
+--		end
+--go
+
+--Xóa Đoi Tác
+create procedure deleteDT
+	@MaDT int
+as
+begin tran
+	delete from HOPDONG where MaDT = @MaDT
+	delete from DOITAC where MaDT = @MaDT
+commit tran
+go
+
+
+
+
---
+++ SqlLoi_Thien/DirtyRead/DirtyRead_Fix.sql
@@ -0,0 +1,62 @@
+﻿use DBMS_ThucHanh_Nhom15
+go
+----------------------------------------------
+-- Transaction 1:  Nhân viên giảm giá cho các sản phẩm được cung cấp bởi chi nhánh 
+alter procedure NV_giamgia_SP2
+	@MaCN nvarchar(10),
+	@GiaGiam int
+as
+begin tran 
+    if not exists (select * from CHINHANH where @MaCN = MaCN)
+    begin
+        raiserror (N'Chi nhánh không tồn tại', 10, 1);
+		rollback tran
+    end
+
+	else
+	begin
+		update SANPHAM
+		set Gia = Gia - @GiaGiam
+		from SANPHAM sp, CUNGCAP_SP cc
+		where sp.MaSP = cc.MaSP and cc.MaCN = @MaCN 
+		waitfor delay '00:00:10'
+	end
+
+	if @GiaGiam >= 20000
+	begin
+		raiserror (N'Giá giảm vượt quá 20000', 10, 1);
+		rollback tran
+	end
+
+	else 
+	begin
+		if @GiaGiam < 0
+		begin
+			raiserror (N'Giá giảm không hợp lệ', 10, 1);
+			rollback tran
+		end
+		else 
+			commit tran
+	end
+go
+
+-- Transaction 2: Khách hàng xem các sản phẩm được cung cấp bởi chi nhánh 
+alter procedure XemSP_CuaCN2
+	@MaCN nvarchar(10)
+as
+begin tran
+	SET TRAN ISOLATION LEVEL READ COMMITTED		-- sửa ở đây
+	if not exists (select * from CHINHANH where @MaCN = MaCN)
+	begin
+		raiserror (N'Chi nhánh không tồn tại', 10, 1);
+		rollback tran
+	end
+	
+	else
+	begin
+		select * 
+		from CUNGCAP_SP cc, SANPHAM sp 
+		where sp.MaSP = cc.MaSP and cc.MaCN = @MaCN 
+		commit tran
+	end
+go
